3f2514adbf2b24221b489b4c11305a5b941942e0
Nando717 <iverlandioedsonn@gmail.com>
2025-01-17 16:58:56 -0300

    criei o  metodo para enviar mensagens

--- email/ms-Email/src/main/java/com/microservice/ms_Email/services/EmailService.java
+++ email/ms-Email/src/main/java/com/microservice/ms_Email/services/EmailService.java
@@ -1,7 +1,53 @@
 package com.microservice.ms_Email.services;
 
+import com.microservice.ms_Email.entity.EmailModel;
+import com.microservice.ms_Email.entity.StatusEmail;
+import com.microservice.ms_Email.repositories.EmailRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
+
+    @Autowired
+    EmailRepository emailRepository;
+
+    @Autowired
+    JavaMailSender emailSender;
+
+
+    @Value(value = "${spring.mailusername}")
+    private String emailFrom;
+
+
+    @Transactional
+    public EmailModel sendMessage(EmailModel emailModel){
+
+        try {
+            emailModel.setSendDateEmail(LocalDateTime.now());
+            emailModel.setEmailFrom(emailFrom);
+
+
+            SimpleMailMessage message = new SimpleMailMessage(); // objeto java que envia emails simples.
+            message.setTo(emailModel.getEmailTo());     // conteudo que vamos enviar
+            message.setSubject(emailModel.getSubject());
+            message.setText(emailModel.getText());
+            emailSender.send(message); // carteiro que envia a mensagem
+
+            emailModel.setStatusEmail(StatusEmail.SENT);
+        }catch (MailException e){
+            emailModel.setStatusEmail(StatusEmail.ERROR);
+        } finally {
+            return emailRepository.save(emailModel);
+        }
+    }
 }
+
+
